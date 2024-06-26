c096f8fcbc625d21d6ce7cc199c6a8e9c07f7d8b
thamyis99 <thamyis99@gmail.com>
2024-06-26 20:13:14 +0700

    add scheduler and firebase

---
+++ auth/src/main/java/com/core/auth/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.core.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
